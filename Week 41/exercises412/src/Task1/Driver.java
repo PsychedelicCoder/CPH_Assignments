11f2d8173c8407e05e237f3e26def611cfad5f6e
PsychedelicCoder <PsychedelicCoder@users.noreply.github.com>
2020-10-08 16:04:06 +0200

    Aflevering

---
+++ Week 41/exercises412/src/Task1/Driver.java
@@ -0,0 +1,19 @@
+package Task1;
+
+public class Driver {
+
+    private String name;
+    private int age;
+
+
+public Driver (String name, int age) {
+    this.name = name;
+    this.age = age;
+}
+
+    @Override
+    public String toString() {
+        return "The name of the driver is: " + name + "\n" + "The ahe of the driver is: " + age;
+        }
+    }
+
---
+++ Week 41/exercises412/src/Task3/Zoo.java
@@ -0,0 +1,21 @@
+package Task3;
+
+import java.util.ArrayList;
+
+public class Zoo {
+
+    void makeAllSounds() {
+
+
+    }
+
+    Animal animal1 = new Animal(3);
+    Animal animal2 = new Animal(5);
+    Animal animal3 = new Animal(6);
+
+    ArrayList arrayList = new ArrayList();
+        arrayList.add (animal1);
+        arrayList.add (animal2);
+        arrayList.add (animal3);
+
+}
